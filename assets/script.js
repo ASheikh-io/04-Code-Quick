181703f415155587947e50551b3e1e4fabed1f99
Ahmed Sheikh <66457087+ASheikh-io@users.noreply.github.com>
2020-12-01 00:51:35 -0500

    getting the questions to show

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
 const homeScreen = document.querySelector("#home");
+const rules = document.querySelector(".rules");
 const startButton = document.getElementById("start-btn");
-const quizContainer = document.querySelectorAll(".quiz-container");
-const question = document.querySelector("#question");
+const quizContainer = document.querySelector(".quiz-container");
+const questionText = document.querySelector("#question");
 const choices = Array.from(document.querySelectorAll(".choice-text"));
-const progressText = document.querySelector("#progressText");
-const score = document.querySelector("#score");
-const progressBarFull = document.querySelector("#progressBarFull");
+const questionHeader = document.querySelector(".question-header");
+
+const displayBox = document.querySelector(".display-box");
+const displayLabel = document.querySelector("#display-label");
+const timerDisplay = document.querySelector("#timer-display");
 
 let currentQuestion = {};
 let acceptingAnswers= true;
-// let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
+let currentAnswer = 0;
+let questionIncrement = 0;
+
+let storedHighScores = JSON.parse(localStorage.getItem("highScores"));
+
 let questions = [{
+        questionNumber: "Question One",
         question: "What is the HTML tag under which one can write the JavaScript code?",
         answer: 3,
         choices: [
@@ -27,6 +35,7 @@ let questions = [{
     },
 
     {
+        questionNumber: "Question Two",
         question: "Which of the following is the correct syntax to display “JS Code Quiz” in an alert box using JavaScript?",
         answer: 4,
         choices: [
@@ -39,6 +48,7 @@ let questions = [{
         ],
     },
     {
+        questionNumber: "Question Three",
         question: "Select the correct syntax to create a 'For Loop.'",
         answer: 1,
         choices: [
@@ -50,6 +60,7 @@ let questions = [{
             "None of the above"],
     },
     {
+        questionNumber: "Question Four",
         question: "Arrays in Javascript can be used to store _____.",
         answer: 5,
         choices: [
@@ -61,6 +72,7 @@ let questions = [{
             "None of the above"]
     },
     {
+        questionNumber: "Question Five",
         question: "A very useful tool used during development and debugging for printing content to the debugger is:?",
         answer: 4,
         choices: [
@@ -73,32 +85,56 @@ let questions = [{
     }
 ];
 
-const SCORE_POINTS = 100;
-const MAX_QUESTIONS = 5;
+
+// Timer
+let startTime = 180;
+let countDownInterval;
+
+function startTimer() {
+  countDownInterval = setInterval(function () {
+    startTime -= 1;
+
+    let minutes = Math.floor(startTime / 60);
+    let seconds = Math.floor(startTime % 60);
+
+    if (seconds < 10) {
+      timerDisplay.innerHTML = minutes + ":0" + seconds;
+    } else {
+      timerDisplay.innerHTML = minutes + ":" + seconds;
+    }
+    if (startTime < 0) {
+      clearInterval(countDownInterval);
+      timerDisplay.textContent = "EXPIRED";
+      displayBox.setAttribute("class", "incorrect");
+      startTime = 0;
+    }
+  }, 1000);
+  console.log(countDownInterval);
+}
+
+
+function askQuestions() {
+
+    // Write the question title and actual question
+    questionHeader.textContent = questions[questionIncrement].questionNumber;
+    questionText.textContent = questions[questionIncrement].question;
+  
+    // Loop to add the answer options
+    for (let x = 0; x < choices.length; x++) {
+      choices[x].innerHTML = questions[questionIncrement].choices[x];
+    }
+    console.log(choices);
+  }
+
 
 // Start Game
 
 function startGame() {
     console.log("clicked")
-    homeScreen.setAttribute("class", "hide-card" );
+    homeScreen.setAttribute("class", "hide-card");
     quizContainer.setAttribute("class", "show-card");
-}
-
-
-function getNewQuestion() {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score);
-
-        return window.location.assign()
-    }
-
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-
-    const questionsIndex = Math.floor(Math.random() & availableQuestions.length);
-    currentQuestion = availableQuestions[questionsIndex];
-    question.innerText = currentQuestion.question;
+    askQuestions();
+    startTimer();
 }
 
 
